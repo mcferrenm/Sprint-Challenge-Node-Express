b53843dfef0d1f6293b8f3032081ebf406991ea1
mcferrenm <mcferrenmax@gmail.com>
2019-02-15 11:26:40 -0500

    Add global middleware

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const cors = require('cors')
 
 const server = express();
+
+// Global Middleware
+server.use(cors());
+server.use(helmet());
+server.use(morgan('dev'));
+server.use(express.json());
 
 module.exports = server;
